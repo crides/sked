8a108958462035ed11f8c2f7554a169a2534771f
crides <zhuhaoqing@live.cn>
2020-08-12 22:09:30 -0500

    Use simple event handling in Lua

--- lua/init.lua
+++ lua/init.lua
@@ -1,2 +1,21 @@
-sched.log.new("System init", ":init")
+event_handlers = {}
+function add_handler(pat, handler)
+    table.insert(event_handlers, {pat, handler})
+    pprint(event_handlers)
+end
+function sched.log.new(typ, map)
+    sched.log._new(typ, map)
+    for _, handler in ipairs(event_handlers) do
+        if string.match(typ, handler[1]) then
+            handler[2](typ, map)
+        end
+    end
+end
+
+add_handler(":.*:", function(typ, map)
+    print("type:", typ)
+    print("attrs:", map)
+end)
+
+sched.log.new(":init:")
 repl()
